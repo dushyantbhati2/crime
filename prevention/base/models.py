3f082f2d47727458edb72ded36035922d051ba88
dushyantbhati2 <db90054@gmail.com>
2024-03-22 19:05:11 +0530

    checked

--- prevention/base/models.py
+++ prevention/base/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class test(models.Model):
+    user=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user
+
+class test2(models.Model):
+    user=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user 
