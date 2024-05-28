c25d4135a295ad13328bc4194180fb495c0e7411
dushyantbhati2 <db90054@gmail.com>
2024-05-28 13:47:37 +0530

    Co-authored-by: Ramanand87 <Ramanand87@users.noreply.github.com> Co-authored-by: Ridhesh Chauhan <Ridhesh05@users.noreply.github.com>

--- prevention/base/admin.py
+++ prevention/base/admin.py
@@ -3,9 +3,6 @@ from . import models
 # Register your models here.
 
 admin.site.register(models.Profile)
-<<<<<<< HEAD
-=======
 admin.site.register(models.Post)
 admin.site.register(models.Comments)
 admin.site.register(models.PostFile)
->>>>>>> frontend
