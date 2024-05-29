0d9e2d5e70136f4b2d582eba15c6b2f002dd422c
dushyantbhati2 <db90054@gmail.com>
2024-05-29 19:20:46 +0530

    home page started

--- client/src/Components/Header/CardSection.jsx
+++ client/src/Components/Header/CardSection.jsx
@@ -1,22 +1,117 @@
-import React from 'react'
+import React from "react";
 
 const CardSection = () => {
   return (
     <>
-      <div className='flex justify-center gap-40 items-center mt-20 sm:px-16'>
-        <div className='w-[450px] flex flex-col gap-6'>
-        <h1 className='text-5xl font-bold '>
-      Track crime in real-time
-      </h1>
-      <p className='text-lg  text-zinc-800 '>Discover our safety features</p>
-      <button className='p-2 w-full border border-zinc-700  text-zinc-800'>Explore now</button>
+      <div className="m-10">
+        <div className="flex justify-between items-center ">
+          <div className="w-[450px] ml-[200px]  flex flex-col gap-10">
+            <h1 className="text-5xl font-bold w-[330px] ">
+              Track crime in real-time
+            </h1>
+            <p className="text-lg  text-zinc-800 ">
+              Discover our safety features
+            </p>
+            <button className="p-2 w-full border border-zinc-700  text-zinc-800 hover:bg-white hover:scale-105 hover:ease-out duration-100">
+              Explore now
+            </button>
+          </div>
+          <div className=" w-[600px] shadow-lg">
+            <img
+              className="h-[400px] "
+              src="https://i.blogs.es/635f55/maps/1366_2000.jpg"
+              alt=""
+            />
+          </div>
         </div>
-        <div className=' w-96 shadow-lg'>
-            <img src="https://i.blogs.es/635f55/maps/1366_2000.jpg" alt="" />
+        <div className="flex items-center justify-center flex-col mt-12">
+          <h5>Trusted by communities worldwide</h5>
+          <div className="flex flex-row gap-12 text-2xl font-bold p-6 pb-10">
+            <h2>Safety</h2>
+            <h2>Safety</h2>
+            <h2>Safety</h2>
+            <h2>Safety</h2>
+          </div>
+        </div>
+
+        {/* <div className="bg-white w-[96%] flex justify-evenly items-center">
+            <div>
+              <img className="w-[200px] h-[200px]" src="" alt="" />
+            </div>
+            <div className="w-[40%]">
+              <h2 className="text-3xl font-semibold mt-4 mb-8">Heading</h2>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint
+                enim ut incidunt earum temporibus sed maxime necessitatibus
+                suscipit in quos aliquam ipsum veritatis eveniet debitis
+                asperiores ducimus nam, porro modi!
+              </p>
+              <button className="p-2 mt-6 w-[40%] border border-zinc-700  text-zinc-800 hover:scale-105 hover:ease-out duration-100 hover:bg-[#F0F0F0]">
+                Explore now
+              </button>
+            </div>
+          </div> */}
+        <div className="bg-white w-[99.99%] flex flex-col gap-10">
+          <div className="flex justify-evenly items-center mt-[20px]">
+            <div>
+              <img className="w-[200px] h-[200px]" src="" alt="" />
+            </div>
+            <div className="w-[40%]">
+              <h2 className="text-3xl font-semibold mt-4 mb-8">Heading</h2>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint
+                enim ut incidunt earum temporibus sed maxime necessitatibus
+                suscipit in quos aliquam ipsum veritatis eveniet debitis
+                asperiores ducimus nam, porro modi!
+              </p>
+              <button className="p-2 mt-6 w-[40%] border border-zinc-700  text-zinc-800 hover:scale-105 hover:ease-out duration-100 hover:bg-[#F0F0F0]">
+                Explore now
+              </button>
+            </div>
+          </div>
+          <div className="flex justify-evenly items-center mt-[60px]">
+            <div className="w-[40%]">
+              <h2 className="text-3xl font-semibold mt-4 mb-8">Heading</h2>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint
+                enim ut incidunt earum temporibus sed maxime necessitatibus
+                suscipit in quos aliquam ipsum veritatis eveniet debitis
+                asperiores ducimus nam, porro modi!
+              </p>
+              <button className="p-2 mt-6 w-[40%] border border-zinc-700  text-zinc-800 hover:scale-105 hover:ease-out duration-100 hover:bg-[#F0F0F0]">
+                Explore now
+              </button>
+            </div>
+            <div>
+              <img className="w-[200px] h-[200px]" src="" alt="" />
+            </div>
+          </div>
+          <div className="flex justify-evenly items-center mt-[60px]">
+            <div>
+              <img className="w-[200px] h-[200px]" src="" alt="" />
+            </div>
+            <div className="w-[40%]">
+              <h2 className="text-3xl font-semibold mt-4 mb-8">Heading</h2>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint
+                enim ut incidunt earum temporibus sed maxime necessitatibus
+                suscipit in quos aliquam ipsum veritatis eveniet debitis
+                asperiores ducimus nam, porro modi!
+              </p>
+              <button className="p-2 mt-6 w-[40%] border border-zinc-700  text-zinc-800 hover:scale-105 hover:ease-out duration-100 hover:bg-[#F0F0F0]">
+                Explore now
+              </button>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div>
+            <h1>Visit our forum</h1>
+          </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default CardSection
+export default CardSection;
--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 import CardSection from "./CardSection";
+import logo from "../../assets/black signup1.jpeg.jpg"
 
 export default function Header() {
   const [auth, setAuth] = useAuth();
@@ -25,22 +26,29 @@ export default function Header() {
 
   return (
     <>
-    <div className="flex justify-between h-16">
-      <div className="bg-[#576066] w-[200px] text-white p-2 flex justify-center ">
+    <div className="flex justify-between h-16 items-center">
+      <div className="bg-[#F0F0F0] w-[200px] text-white p-2 flex justify-center ">
         {/* <Link className="hover:bg-[#122932] hover:rounded-lg hover:scale-105 hover:ease-out duration-100 px-3 py-2">Crime</Link> */}
       </div>
-      <div className="w-[calc(100%-500px)] h-16 flex items-center">
-        <ul className=" px-1 text-base space-x-1  gap-7  flex align-center font-semibold">
-          <li className="hover:bg-[#6250FE] hover:rounded-lg hover:scale-105 hover:ease-out duration-100 p-2"><Link to="/">Home</Link></li>
-          <li className=" hover:rounded-lg hover:scale-105 hover:ease-out duration-100 p-2"><Link to="/">Map</Link></li>
-          <li className=" hover:rounded-lg hover:scale-105 hover:ease-out duration-100 p-2"><Link to="/allprojects">All Projects</Link></li>
-          <li className=" hover:rounded-lg hover:scale-105 hover:ease-out duration-100 p-2"><Link to="">About</Link></li>
-          <li className="hover:rounded-lg hover:scale-105 hover:ease-out duration-100 p-2"><Link to="">Contact</Link></li>
+      <div className="h-16 w-[150px] bg-white flex items-center font-semibold overflow-hidden">
+      <div className="  hover:scale-105 hover:ease-out duration-100 px-3 py-2 bg-white ml-8">
+      <img src={logo} className="rounded-full w-[50px] h-[50px] "/>
+      </div>
+      </div>
+      <div className="w-[calc(100%-500px)] h-16 flex items-center bg-white justify-center font-semibold">
+        
+        <ul className=" px-1 text-base space-x-1  gap-6  flex align-center ">
+          <li className="hover:bg-[#F0F0F0] hover:rounded-lg hover:scale-105 hover:ease-out duration-100 p-2"><Link to="/">Home</Link></li>
+          <li className="hover:bg-[#F0F0F0] hover:rounded-lg hover:scale-105 hover:ease-out duration-100 p-2"><Link to="/">Map</Link></li>
+          <li className="hover:bg-[#F0F0F0]  hover:rounded-lg hover:scale-105 hover:ease-out duration-100 p-2"><Link to="/allprojects">All Projects</Link></li>
+          <li className="hover:bg-[#F0F0F0]  hover:rounded-lg hover:scale-105 hover:ease-out duration-100 p-2"><Link to="">About</Link></li>
+          <li className="hover:bg-[#F0F0F0] hover:rounded-lg hover:scale-105 hover:ease-out duration-100 p-2"><Link to="">Contact</Link></li>
         </ul>
       </div>
-      <div className="">
-        <Link to="" className="w-[100px] ">Login</Link>
-        <Link to="" className="w-[100px]">Signup</Link></div>
+      <div className="bg-white w-[300px] h-16 flex items-center font-semibold">
+        <button className="w-[120px] h-12 border-l-2 border-t-2 border-b-2 border-zinc-300 hover:scale-105 hover:ease-out duration-100 "><Link to="" >Login</Link></button>
+        <button className="w-[120px] h-12 border-2 border-zinc-300 bg-[#F0F0F0]  hover:bg-white hover:scale-105 hover:ease-out duration-100"><Link to="" >Signup</Link></button>
+        </div>
   </div>
 
     </>
