9cd5ae6da787e423546344f557724bb80ae0f871
Ramanand87 <ram3036anand03@gmail.com>
2024-05-30 16:51:47 +0530

    home page components and footer added

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -1,10 +1,53 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
-
+// import { Link } from 'react-router-dom';
+import youtube from "../../assets/icons/youtube.jpg"
+import instagram from "../../assets/icons/instagram.png"
+import github from "../../assets/icons/github.png"
+import twitter from "../../assets/icons/twitter.png"
+import logo from "../../assets/black signup1.jpeg.jpg"
 export default function Footer() {
     return (
-        <footer className="bg-white border-y">
-            
+        <footer className="bg-white border-y flex h-[200px] w-full justify-around items-center">
+            <div className="flex flex-col gap-4 justify-center w-[300px] items-center">
+                <div className="flex flex-col justify-center items-start mr-5 ">
+                <img src={logo} className="rounded-full w-[50px] h-[50px]"/>
+                <h1 className="font-bold text-2xl ">SafeZone</h1>
+                </div>
+                <div>
+                <p className="text-md">2024 @ SafeZone</p>
+                <p className="text-md"> All Rights Reserved</p>
+                </div>
+            </div>
+            <div className="w-[300px]">
+                <ul className="flex flex-col font-semibold gap-3">
+                <li>Map</li>
+                <li>Community</li>
+                <li>About us</li>
+                <li>Contact us</li>
+                </ul>
+            </div>
+            <div>
+                <ul className="flex flex-col font-semibold gap-3">
+                <li>Resources</li>
+                <li>Safety</li>
+                <li>Colloborations & Awareness</li>
+                <li>Emergency Button</li>
+                </ul>
+            </div>
+            <div className="w-[300px] flex h-[150px] items-end gap-4">
+            <div class="flex justify-center items-center bg-white w-16 h-16 rounded-full">
+                <img src={youtube} alt="Lock Icon" class="w-9 h-9" />
+              </div>
+              <div class="flex justify-center items-center bg-white w-16 h-16 rounded-full ">
+                <img src={instagram} alt="Lock Icon" class="w-9 h-9" />
+              </div>
+              <div class="flex justify-center items-center bg-white w-16 h-16 rounded-full">
+                <img src={github} alt="Lock Icon" class="w-9 h-9" />
+              </div>
+              <div class="flex justify-center items-center bg-white w-16 h-16 rounded-full">
+                <img src={twitter} alt="Lock Icon" class="w-9 h-9" />
+              </div>
+            </div>
         </footer>
     );
 }
--- client/src/Components/Header/CardSection.jsx
+++ client/src/Components/Header/CardSection.jsx
@@ -1,5 +1,15 @@
 import React from "react";
-
+import lock1 from "../../assets/icons/lock.png"
+import chat1 from "../../assets/icons/chat.png"
+import handshake1 from "../../assets/icons/handshake.png"
+import link1 from "../../assets/icons/link.png"
+import fork from "../../assets/icons/fork.png"
+import zoomin1 from "../../assets/icons/zoomin.png"
+import zoomout1 from "../../assets/icons/zoomout.png"
+import upload1 from "../../assets/icons/upload.png"
+import img1 from "../../assets/imgforhome4.jpg"
+import img2 from "../../assets/imgforhome2.jpg"
+import img3 from "../../assets/imgforhome3.jpg"
 const CardSection = () => {
   return (
     <>
@@ -33,80 +43,87 @@ const CardSection = () => {
             <h2>Safety</h2>
           </div>
         </div>
-
-        {/* <div className="bg-white w-[96%] flex justify-evenly items-center">
-            <div>
-              <img className="w-[200px] h-[200px]" src="" alt="" />
-            </div>
-            <div className="w-[40%]">
-              <h2 className="text-3xl font-semibold mt-4 mb-8">Heading</h2>
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint
-                enim ut incidunt earum temporibus sed maxime necessitatibus
-                suscipit in quos aliquam ipsum veritatis eveniet debitis
-                asperiores ducimus nam, porro modi!
-              </p>
-              <button className="p-2 mt-6 w-[40%] border border-zinc-700  text-zinc-800 hover:scale-105 hover:ease-out duration-100 hover:bg-[#F0F0F0]">
-                Explore now
-              </button>
-            </div>
-          </div> */}
         <div className="bg-white w-[99.99%] flex flex-col gap-10">
           <div className="flex justify-evenly items-center mt-[20px]">
             <div>
-              <img className="w-[200px] h-[200px]" src="" alt="" />
+              <img className="w-[350px] h-[250px]" src={img1} alt="" />
             </div>
             <div className="w-[40%]">
-              <h2 className="text-3xl font-semibold mt-4 mb-8">Heading</h2>
+              <h2 className="text-3xl font-semibold mt-4 mb-8">Intuitive and user-friendly design</h2>
               <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint
-                enim ut incidunt earum temporibus sed maxime necessitatibus
-                suscipit in quos aliquam ipsum veritatis eveniet debitis
-                asperiores ducimus nam, porro modi!
-              </p>
-              <button className="p-2 mt-6 w-[40%] border border-zinc-700  text-zinc-800 hover:scale-105 hover:ease-out duration-100 hover:bg-[#F0F0F0]">
+                We offer a variety of safety services, including live crime tracking, community collaboration, and emergency assistance.              </p>
+              {/* <button className="p-2 mt-6 w-[40%] border border-zinc-700  text-zinc-800 hover:scale-105 hover:ease-out duration-100 hover:bg-[#F0F0F0]">
                 Explore now
-              </button>
+              </button> */}
             </div>
           </div>
           <div className="flex justify-evenly items-center mt-[60px]">
             <div className="w-[40%]">
-              <h2 className="text-3xl font-semibold mt-4 mb-8">Heading</h2>
+              <h2 className="text-3xl font-semibold mt-4 mb-8">Simplicity is key</h2>
               <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint
-                enim ut incidunt earum temporibus sed maxime necessitatibus
-                suscipit in quos aliquam ipsum veritatis eveniet debitis
-                asperiores ducimus nam, porro modi!
+                Our straightforward approach to safety ensures quick access to essential resources. We prioritize user-friendly solutions for effective crime prevention.
               </p>
-              <button className="p-2 mt-6 w-[40%] border border-zinc-700  text-zinc-800 hover:scale-105 hover:ease-out duration-100 hover:bg-[#F0F0F0]">
+              {/* <button className="p-2 mt-6 w-[40%] border border-zinc-700  text-zinc-800 hover:scale-105 hover:ease-out duration-100 hover:bg-[#F0F0F0]">
                 Explore now
-              </button>
+              </button> */}
             </div>
             <div>
-              <img className="w-[200px] h-[200px]" src="" alt="" />
+              <img className="w-[350px] h-[250px]" src={img2} alt="" />
             </div>
           </div>
-          <div className="flex justify-evenly items-center mt-[60px]">
+          <div className="flex justify-evenly items-center mt-[60px] mb-[60px]">
             <div>
-              <img className="w-[200px] h-[200px]" src="" alt="" />
+              <img className="w-[350px] h-[250px]" src={img3} alt="" />
             </div>
             <div className="w-[40%]">
-              <h2 className="text-3xl font-semibold mt-4 mb-8">Heading</h2>
+              <h2 className="text-3xl font-semibold mt-4 mb-8">Customized solutions for all</h2>
               <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint
-                enim ut incidunt earum temporibus sed maxime necessitatibus
-                suscipit in quos aliquam ipsum veritatis eveniet debitis
-                asperiores ducimus nam, porro modi!
+              Whether you're an individual or a community organization, we have safety measures to suit your needs. We tailor our services to ensure maximum protection.
               </p>
-              <button className="p-2 mt-6 w-[40%] border border-zinc-700  text-zinc-800 hover:scale-105 hover:ease-out duration-100 hover:bg-[#F0F0F0]">
+              {/* <button className="p-2 mt-6 w-[40%] border border-zinc-700  text-zinc-800 hover:scale-105 hover:ease-out duration-100 hover:bg-[#F0F0F0]">
                 Explore now
-              </button>
+              </button> */}
             </div>
           </div>
         </div>
         <div>
-          <div>
-            <h1>Visit our forum</h1>
+          <div class="mt-[100px] flex flex-row justify-evenly p-8">
+            <div class="text-center mb-8">
+              <h1 class="text-2xl font-semibold mb-8">Connect with other users</h1>
+              <p class="text-gray-600">Explore SafeZone's integration options for seamless user experience.</p>
+              <button className="p-2 mt-10 w-[200px] border border-zinc-700  text-zinc-800 hover:bg-white hover:scale-105 hover:ease-out duration-100">
+                Connect apps
+              </button>
+            </div>
+            <div class="grid grid-cols-3 w-[400px]">
+              <div class="flex justify-center items-center bg-white w-[80px] h-[80px] rounded-full ml-12 mt-5 shadow-lg">
+                <img src={lock1} alt="Lock Icon" class="w-8 h-8" />
+              </div>
+              <div class="flex justify-center items-center bg-white w-16 h-16 ml-7 rounded-full shadow-lg">
+                <img src={zoomout1} alt="Search Icon" class="w-8 h-8" />
+              </div>
+              <div class="flex justify-center items-center bg-white w-[80px] h-[80px] rounded-full mt-[60px] shadow-lg">
+                <img src={chat1} alt="Chat Icon" class="w-8 h-8" />
+              </div>
+              <div class="flex justify-center items-center bg-white mt-7 w-16 h-16 ml-6 rounded-full shadow-lg">
+                <img src={zoomin1} alt="Zoom Icon" class="w-8 h-8" />
+              </div>
+              <div class="flex justify-center items-center bg-white w-[80px] ml-5 h-[80px] rounded-full shadow-lg">
+                <img src={link1} alt="Link Icon" class="w-8 h-8" />
+              </div>
+              <div class="flex justify-center items-center bg-white w-16 h-16 mt-12 ml-8 rounded-full shadow-lg">
+                <img src={upload1} alt="Upload Icon" class="w-8 h-8" />
+              </div>
+              <div class="flex justify-center items-center bg-white w-[80px] h-[80px] mt-6 rounded-full shadow-lg">
+                <img src={chat1} alt="User Icon" class="w-8 h-8" />
+              </div>
+              <div class="flex justify-center items-center bg-white w-16 h-16 mt-[35px] rounded-full shadow-lg">
+                <img src={handshake1} alt="Connect Ihandshake1con" class="w-8 h-8" />
+              </div>
+              <div class="flex justify-center items-center bg-white  w-[80px] h-[80px] mt-10 rounded-full shadow-lg">
+                <img src={fork} alt="Share Icon" class="w-8 h-8" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
