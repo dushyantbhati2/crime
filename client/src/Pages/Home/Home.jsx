84ffb681a355573090432b5765004b2d030d4e34
dushyantbhati2 <db90054@gmail.com>
2024-05-31 21:48:39 +0530

    home page responsive started

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -3,11 +3,12 @@ import { Link } from "react-router-dom";
 import Layout from "../Layout/Layout";
 import Header from "../../Components/Header/Header";
 import CardSection from "../../Components/Header/CardSection";
-import Footer from "../../Components/Footer/Footer";
 
 export default function Home() {
   return (
    
     <CardSection/>
+
+  
   );
 }
