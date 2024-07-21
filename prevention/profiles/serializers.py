621a733d5e2072e675eed25b38f73cad05ac0460
Ramanand87 <ram3036anand03@gmail.com>
2024-07-21 18:54:00 +0530

    profile backend updated

--- prevention/profiles/serializers.py
+++ prevention/profiles/serializers.py
@@ -1,9 +1,30 @@
 from rest_framework.serializers import ModelSerializer
-
+from rest_framework import serializers
 from .models import Profile
+from communities.models import Post
+from django.contrib.auth.models import User
+from user.serializers import userSerializers
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = (
+            "description",
+            "post_id",
+        )
 
+class ProfileSerializer(serializers.ModelSerializer):
+    user = userSerializers()
+    posts = serializers.SerializerMethodField()
 
-class ProfileSerializer(ModelSerializer):
     class Meta:
         model = Profile
-        fields = "__all__"
+        fields = (
+            "user",
+            "gender",
+            "occupation",
+            "posts",
+        )
+
+    def get_posts(self, obj):
+        user_posts = Post.objects.filter(post_user=obj.user)
+        return PostSerializer(user_posts, many=True).data
